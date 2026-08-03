7a0464ea0fa2d083b99dbe0251653e541c0c5977
MOhamed Gamal mahmoud <mrwhjmal656@gmail.com>
2026-08-03 22:14:56 +0300

    Update Doctor.java

--- Doctor.java
+++ Doctor.java
@@ -1,31 +1,37 @@
 package org.hosbitial.doctorsystem.doctorentity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 @Entity
-@Table(name="Doctor_table")
+@Table(name = "doctor_table")
 public class Doctor {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id1;
-    @Column(name="Doctor_name",nullable = false)
+    private Long id;
+
+    @Column(name = "doctor_name", nullable = false)
     private String name;
-    @Column(name="address",nullable = false)
+
+    @Column(name = "address", nullable = false)
     private String address;
-    @Column(name="doctor_phone",nullable = false,unique = true)
+
+    @Column(name = "doctor_phone", nullable = false, unique = true)
     private String phone;
-    @Column(name = "doctor_gender",nullable = false)
+
+    @Column(name = "doctor_gender", nullable = false)
     private String gender;
-    @Column(name = "doctor_email",nullable = false,unique = true)
-    private  String email;
-    @Column(name="doctor_specialization",nullable = false)
+
+    @Column(name = "doctor_email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "doctor_specialization", nullable = false)
     private String specialization;
-
-
-
 
 }
